1798b2c301ed67e2ba4b543200d6be1899f74cdd
m-t7 <75344022+m-t7@users.noreply.github.com>
2026-08-15 10:49:58 -0400

    Update app.js

--- CS465/travlr/app.js
+++ CS465/travlr/app.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
-var handlebars = require('hbs');
 
 require('./app_api/models/db');
 require('./app_api/models/user');
@@ -21,31 +15,33 @@ require('./app_api/config/passport');
 
 var app = express();
 
-// view engine setup
-app.set('views', __dirname + '/app_server/views');
-
-// Register handlebars partials
-handlebars.registerPartials(__dirname + '/app_server/views/partials');
-
-app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
+// Allowed origins for the frontends that consume this API:
+// - localhost:5173: React + Vite public site (apps/public)
+// - localhost:5174: React + Vite admin dashboard (apps/admin)
+const ALLOWED_ORIGINS = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+];
+
 app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  const origin = req.headers.origin;
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
@@ -56,21 +52,17 @@ app.use(function(req, res, next) {
 // Catch unauthorized error and create 401
 app.use((err, req, res, next) => {
   if(err.name === 'UnauthorizedError') {
-    res
+    return res
       .status(401)
       .json({"message": err.name + ": " + err.message});
   }
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ message: err.message });
 });
 
 module.exports = app;
